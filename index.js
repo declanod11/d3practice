18ef8a92c4d236ff52b66d86a4dc6936c55c6973
declanod11 <declan.j.odonovan@gmail.com>
2020-02-04 11:52:20 +1100

    updated package.json start

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var express = require('express');
 // Create the app
 var app = express();
 
+// app.use(express.static('public'));
+
 // Set up the server
 // process.env.PORT is related to deploying on heroku
 var server = app.listen(process.env.PORT || 3000, listen);
@@ -20,12 +22,14 @@ function listen() {
   console.log('Example app listening at http://' + host + ':' + port);
 }
 
-// Set the route for the root directory
-app.get('/', hello);
 
-// This is what happens when any user requests '/'
-function hello(req, res) {
-  // Just send back "Hello World!"
-  // Later we'll see how we might send back JSON
-  res.send('Hello World!');
-}
+
+// // Set the route for the root directory
+// app.get('/', hello);
+
+// // This is what happens when any user requests '/'
+// function hello(req, res) {
+//   // Just send back "Hello World!"
+//   // Later we'll see how we might send back JSON
+//   res.send('Hello World!');
+// }

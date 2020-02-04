88c995db21b4b903d11d3995b792b06cf5bf682d
declanod11 <declan.j.odonovan@gmail.com>
2020-02-04 13:27:20 +1100

    changed home.html to index.html

--- server.js
+++ server.js
@@ -8,26 +8,33 @@
 var express = require('express');
 // Create the app
 var app = express();
-
-// app.use(express.static('public'));
+var port = 3000;
 
 // Set up the server
 // process.env.PORT is related to deploying on heroku
-var server = app.listen(process.env.PORT || 3000, listen);
+var server = app.listen(port, listen);
+
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//     res.send('Get response');
+// });
 
 // This call back just tells us that the server has started
 function listen() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://' + host + ':' + port);
+  console.log('Listening on port '+port+'!');
 }
 
 
 
-// // Set the route for the root directory
+// Set the route for the root directory
 // app.get('/', hello);
+// app.get("/", (req, res) => {
+//     // res.sendFile(__dirname + '/public/home.html');
+//     app.use('/public', express.static(__dirname + '/public'));
+// });
 
-// // This is what happens when any user requests '/'
+// // // This is what happens when any user requests '/'
 // function hello(req, res) {
 //   // Just send back "Hello World!"
 //   // Later we'll see how we might send back JSON
